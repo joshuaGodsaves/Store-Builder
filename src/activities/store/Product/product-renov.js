b56afd4043f747474282b5405999d85c11c6be08
joshuaGodsaves <joshuaGodsaves19@gmail.com>
2019-08-27 22:49:42 +0100

    Fixed Basic IO in new product components and we are good to launch.

--- src/activities/store/Product/product-renov.js
+++ src/activities/store/Product/product-renov.js
@@ -1,17 +1,32 @@
 import React from "react"
 import withStyles from "@material-ui/core/styles/withStyles"
-import { AddAPhoto, TagFaces } from "@material-ui/icons"
-import { APIURL } from "../../../DataSource"
+import {AddAPhoto} from "@material-ui/icons"
+import {APIURL} from "../../../DataSource"
 import StoreContext from "../StoreContext"
 import {
-    ButtonBase, Avatar, TextField,    MenuItem,                                   
-    Tabs, Tab, Button, Grid, GridList, GridListTile, IconButton, GridListTileBar
-} from "@material-ui/core"
+    Avatar,
+    Button,
+    ButtonBase,
+    Chip,
+    Grid,
+    GridList,
+    GridListTile,
+    GridListTileBar,
+    IconButton,
+    LinearProgress,
+    Menu,
+    MenuItem,
+    Tab,
+    Tabs,
+    Typography
+} from '@material-ui/core';
 import Context from "../../../AppContext"
 import AppInput from "../../../components/AppInput"
+
 import AppPaper from "../../../components/AppPaper"
-import AppExpansionPanel from "../../../components/AppExpansionPanel"
-import CategoryLite from '../components/CategoryLite'
+
+import CategoryLite from "../components/CategoryLite"
+
 import FileUploader from "../components/FileUpload"
 import axios from "axios"
 
@@ -22,34 +37,77 @@ let styles = theme => ({
         borderRadius: "4px",
         margin: "0px 12px"
     }
-})
+});
 
 
 class GeneralTab extends React.Component {
 
-    static contextType = Context
+    static contextType = Context;
 
     state = {
-        categoryOpen: false
-    }
+        categoryOpen: false,
+        brandSelectorAnchorEl: false,
+        openBrand: false,
+        typeAnchorEl: false,
+        uploading: false
+    };
+
+    openBrand = (e) => {
+        e.persist();
+        this.setState({brandSelectorAnchorEl: e.target, openBrand: true});
+    };
+    closeBrand = () => {
+        this.setState({brandSelectorAnchorEl: false});
+    };
+    openType = (e) => {
+        e.persist();
+        this.setState({typeAnchorEl: e.target, openType: true});
+    };
+    closeType = () => {
+        this.setState({typeAnchorEl: false});
+    };
+    uploading = () => {
+        this.setState({uploading: true})
+    };
+    openCategory = () => {
+        this.setState(state => {
+            state.categoryOpen = true;
+            return state;
+        })
+    };
+    closeCategory = () => {
+        this.setState({categoryOpen: false})
+    };
+
+    categoryChange = (v) => {
+        this.props.categoryChange(v);
+        this.closeCategory()
+    };
 
     render() {
-        let { classes } = this.props
 
-        let { product, brandChange, categoryChange, tagsChange, watchText, selectMainImage , openCategory } = this.props
+        let {brandSelectorAnchorEl, typeAnchorEl} = this.state;
 
-        let CategoryLite = (
-            <CategoryLite open={this.state.categoryOpen}
-                categories={product.categories.length ? this.state.product.categories : []}
-                closeCategorySelector={this.categoryChange} />
-        )
+        let {classes, product, categoryChange, tagsChange, watchText, selectMainImage, brandChange, brands} = this.props;
 
+        const productTypeMenu = (
+            <Menu anchorEl={typeAnchorEl} open={typeAnchorEl ? true : false} onClose={this.closeType}>
+                <MenuItem onClick={this.closeType}>Type 1</MenuItem>
+                <MenuItem onClick={this.closeType}>Type 2</MenuItem>
+            </Menu>
+        );
         return (
             <>
-                {categoryChange ? CategoryLite : "" }
-                <AppPaper>
+                {this.state.categoryOpen ? (
+                    <CategoryLite open={true}
+                                  categories={product.categories}
+                                  closeCategorySelector={this.categoryChange}/>
+                ) : ""}
+
+
+                <AppPaper nomargin title={"Basic details"}>
                     <Grid container alignContent={"stretch"} spacing={16}>
-                        <Grid item xs={6} style={{ margin: "16px 0px" }}>
+                        <Grid item xs={6} style={{margin: "12px 0px"}}>
                             <div>
                                 <AppInput label={"Product name"} onChange={watchText("title")} value={product.title} />
                                 <AppInput label={"Product caption"} onChange={watchText("caption")} value={product.caption} />
@@ -67,15 +125,21 @@ class GeneralTab extends React.Component {
                             <div style={{ border: ".5px solid ghostwhite" }}>
                                 <GridList style={{ flexDirection: "row", flexWrap: "nowrap", padding: "8px 0px", borderRadius: 4 }} >
                                     <GridListTile className={classes.imageBox}>
-
-
+                                        {this.state.uploading ? <LinearProgress style={{zIndex: 2000}}/> : ""}
                                         {/*  */}
                                         <FileUploader
                                             onError={() => { }}
-                                            onFinish={selectMainImage} />
+                                            onUploading={this.uploading}
+                                            onFinish={(v) => {
+                                                this.setState({uploading: false});
+                                                alert("ok");
+                                                selectMainImage(v)
+                                            }}/>
+
+                                        <img src={product.image} style={{height: "100%"}}/>
 
                                         <GridListTileBar title={"Image"} subtitle={"main product image"} actionIcon={
-                                            // Triggers internal input element [type: file] 
+                                            // Triggers internal input element [type: file]
                                             <IconButton style={{ color: 'white' }} onClick={() => { document.getElementById("fileSelectorElement").click() }}>
                                                 <AddAPhoto />
                                             </IconButton>
@@ -94,34 +158,72 @@ class GeneralTab extends React.Component {
 
                 <Grid container alignContent={"stretch"} spacing={16}>
                     <Grid item xs={6} style={{ margin: "16px 0px" }}>
-                        <AppPaper nomargin>
+                        <AppPaper nomargin title={"Basic classification"}>
                             <div>
-                                <div style={{ height: 40, background: "gold", marginBottom: 12 }}>
-                                    <TextField select variant={"outlined"} value={"Product category"}>
-                                        {[1,2,2,4,5].map(v=>(
-                                            <MenuItem key={v} value={v}>Option {v} </MenuItem>
-                                        ))}
-                                    </TextField>
-                                </div>  
+                                <div style={{marginBottom: 12, background: "ghostwhite", minHeight: 80}}>
+                                    {product.categories.map(v => (
+                                        <Chip label="Category name" style={{margin: "8px 8px"}}/>
+                                    ))}
+                                </div>
                                 <div>
-
-                                    <Button>Product category</Button>
+                                    <ButtonBase onClick={this.openCategory}
+                                                style={{
+                                                    padding: "4px 12px",
+                                                    borderRadius: 4,
+                                                    background: "rgba(0,0,0,.2)",
+                                                    width: "100%"
+                                                }}>
+                                        <Typography variant={"button"}>Product category</Typography>
+                                    </ButtonBase>
                                 </div>
                             </div>
-                            <Grid container style={{ marginTop: "12px" }}>
+                            <Grid container style={{marginTop: "12px"}} spacing={8}>
                                 <Grid item xs={6}>
-                                    <ButtonBase style={{ padding: "4px 12px", borderRadius: 4, background: "rgba(0,0,0,.2)" }}>
-                                        Brand <Avatar style={{ margin: "0px 12px" }} />
+                                    <Menu anchorEl={brandSelectorAnchorEl}
+                                          open={!brandSelectorAnchorEl ? false : true}
+                                          onClose={this.closeBrand}>
+                                        {brands.map(v => (
+                                            <MenuItem onClick={() => {
+                                                brandChange(v);
+                                                this.closeBrand()
+                                            }}>
+                                                <Avatar/>
+                                                <Typography style={{marginLeft: "12px"}}> Brand name </Typography>
+                                            </MenuItem>
+                                        ))}
+                                    </Menu>
+
+                                    <ButtonBase
+                                        onClick={this.openBrand}
+                                        style={{
+                                            padding: "4px 12px",
+                                            borderRadius: 4,
+                                            background: "rgba(0,0,0,.2)",
+                                            width: "100%",
+                                            display: "flex",
+                                            justifyContent: "space-between"
+                                        }}>
+                                        <Avatar style={{margin: "0px 12px"}}/> Brand
                                     </ButtonBase>
                                 </Grid>
                                 <Grid item xs={6}>
-                                    <ButtonBase>
+                                    {productTypeMenu}
+                                    <ButtonBase
+                                        onClick={this.openType}
+                                        style={{
+                                            padding: "4px 12px",
+                                            borderRadius: 4,
+                                            background: "rgba(0,0,0,.2)",
+                                            width: "100%",
+                                            display: "flex",
+                                            justifyContent: "space-between"
+                                        }}>
+                                        <Avatar style={{margin: "0px 12px"}}/> Type
                                     </ButtonBase>
                                 </Grid>
                             </Grid>
-
-                            <AppInput label={"Tags"} nomargin />
-
+                            <AppInput label={"Tags"} nomargin onChange={tagsChange}
+                                      value={product.tags ? product.tags.join(" ") : ""}/>
                         </AppPaper>
                     </Grid>
                     <Grid item xs={6} style={{ margin: "16px 0px" }} hidden>
@@ -137,25 +239,24 @@ class GeneralTab extends React.Component {
 
 class Component extends React.Component {
 
-    static contextType = StoreContext
+    static contextType = StoreContext;
 
 
     state = {
         tabIndex: 0
-    }
-
-    tabChange = (v) => {
-        this.setState({ tabIndex: v });
-    }
-
-
+    };
     state = {
         product: {
             image: null, title: null, caption: null, description: null,
             categories: [], brand: null, sellingPrice: null, costPrice: null,
             available: null, gallery: [], highPrice: null, lowPrice: null
-        }
-    }
+        },
+        loading: false
+    };
+
+    tabChange = (v) => {
+        this.setState({tabIndex: v});
+    };
 
     // Watches for input element onchange event to send values to particular state properties
     watchText = (prop) => {
@@ -166,29 +267,20 @@ class Component extends React.Component {
                 return state
             })
         }
-    }
-
-    saveCategory = (categories) => {
-        this.setState(state => { state.product.categories = categories; state.openCategory = false; return state })
-    }
-
-
-    openCategory = () => {
-        this.setState({ openCategory: true })
-    }
+    };
 
     selectMainImage = (url) => {
         this.setState(state => {
-            state.product.mainImageLink = url
-            state.product.image = url
+            state.product.mainImageLink = url;
+            state.product.image = url;
             return state
         })
-    }
+    };
 
     updateProduct = () => {
-        let { product, mainProductObj } = this.state
-        let { match: { params } } = this.props
-        this.setState({ saveingProduct: true })
+        let {product, mainProductObj} = this.state;
+        let {match: {params}} = this.props;
+        this.setState({saveingProduct: true, loading: true});
         axios
             .put(
                 `${APIURL}/store/${this.context.store.id}/product/${params.product}`,
@@ -199,11 +291,11 @@ class Component extends React.Component {
                     }
                 }
             ).then(v => {
-                this.setState({ saveingProduct: false })
-                this.setState({ updated: true })
+            this.setState({saveingProduct: false, loading: false});
+            this.setState({updated: true});
                 setTimeout(() => {
                     this.setState({ updated: false })
-                }, 2000)
+                }, 2000);
                 this.loadProduct(params.product)
 
             }).catch(v => console.log(v))
@@ -212,7 +304,7 @@ class Component extends React.Component {
 
 
     save = event => {
-        this.setState({ saveingProduct: true })
+        this.setState({saveingProduct: true});
         axios
             .post(
                 `${APIURL}/store/${this.context.store.id}/product`,
@@ -224,23 +316,47 @@ class Component extends React.Component {
                 }
             ).then(v => {
                 //After saveing product reload with product id
-                this.setState({ saveingProduct: false })
+            this.setState({saveingProduct: false});
                 window.location.replace(`/stores/${this.context.store.id}/products`)
             })
     };
 
 
     loadProduct = async (productID) => {
-        let product = await axios.get(`${APIURL}/store/${this.context.store.id}/product/${productID}`)
+        let product = await axios.get(`${APIURL}/store/${this.context.store.id}/product/${productID}`);
         if (product.data) {
-            this.setState({ product: product.data, mainProductObj: product.data })
+            this.setState({product: product.data, mainProductObj: product.data});
             return true
         }
         return false
 
-    }
+    };
+
+    watchBrand = (value) => {
+        this.setState(state => {
+            state.product.brand = value;
+            return state
+        })
+    };
+
+    watchTags = (event) => {
+        event.persist();
+        let arr = event.target.value.split(" ");
+        this.setState(state => {
+            state.product.tags = arr;
+            return state
+        })
+    };
+
+    watchCategory = (cat) => {
+        this.setState(state => {
+            state.product.categories = cat;
+            return state;
+        })
+    };
+
     componentDidMount() {
-        let { match: { params } } = this.props
+        let {match: {params}} = this.props;
         if (params.product == "new") {
             this.setState({ isNewProduct: true })
             // Init new product
@@ -251,19 +367,21 @@ class Component extends React.Component {
                 } else {
                     // could not load product and error occured somewhere maybe in the network.
                 }
-            })
+            });
             this.setState({ productID: params.product })
             //load product
         }
     }
 
     render() {
-        let { tabIndex } = this.state
+        let {tabIndex, loading} = this.state;
 
-        let { classes } = this.props
+        let {classes} = this.props;
         return (
             <>
-                <Grid container justify={"center"} style={{ background: "darkblue" }}>
+                {loading ? <LinearProgress/> : ""}
+                <div style={{margin: "4px 0px"}}></div>
+                <Grid container justify={"center"} style={{background: ""}}>
                     <Grid item xs={10}>
                         <Grid container>
                             <Grid item xs={12} style={{ display: "flex", justifyContent: "space-between" }}>
@@ -285,11 +403,12 @@ class Component extends React.Component {
                     <Grid item xs={11}>
 
                         <GeneralTab classes={classes} product={this.state.product}
-                            watchText={this.watchText}
-                            brandChange={null}
-                            categoryChange={null}
-                            tagsChange={null}
-                            selectMainImage={this.selectMainImage}
+                                    watchText={this.watchText}
+                                    brandChange={this.watchBrand}
+                                    categoryChange={this.watchCategory}
+                                    tagsChange={this.watchTags}
+                                    selectMainImage={this.selectMainImage}
+                                    brands={[1, 2, 3, 4]}
                         />
 
                     </Grid>
